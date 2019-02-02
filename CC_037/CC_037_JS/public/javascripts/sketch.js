4352befa191d641fc7c3a1569c7bec2265eb01bf
Gong Hyeonsik <hyeonsik.gong@gmail.com>
2019-02-02 23:23:59 +0900

    Coding Challenge 37

---
+++ CC_037/CC_037_JS/public/javascripts/sketch.js
@@ -0,0 +1,42 @@
+let seed;
+let button;
+let srcText;
+let words;
+
+function preload() {
+  srcText = loadStrings('../data/linkin park.txt');
+}
+
+function diastic(seed, words) {
+
+  console.log("in");
+
+  let phrase = "";
+  let idx = 0;
+
+  for(let i = 0 ; i < seed.length ; i++) {
+    let alphabet = seed[i];
+    for(let j = idx ; j < words.length ; j++) {
+      let word = words[j];
+      if(word[i] === alphabet) {
+        phrase += (' ' + word);
+        idx = j + 1;
+        break;
+      }
+    }
+  }
+  return phrase;
+}
+
+function setup() {
+  noCanvas();
+  srcText = join(srcText, ' ');
+  words = splitTokens(srcText,' ,!?"\'.');
+  seed = select("#seed");
+  button = select("#submit");
+
+  button.mousePressed(() => {
+    let phrase = (diastic(seed.value(), words));
+    createP(phrase);
+  });
+}
